36563b1a6bea8b3f1cedf8589206595c85b06e0c
Patricio Gonzalez <patote.gonzalez@gmail.com>
2022-09-29 19:23:41 -0300

    add an interface

--- start/src/utils/api.ts
+++ start/src/utils/api.ts
@@ -1,9 +1,28 @@
 import { OPEN_WEATHER_API_KEY } from './const';
 
 const API = OPEN_WEATHER_API_KEY
-export async function fetchOpenWeatherData(city: string): Promise<any> {
+
+export interface OpenWeatherData {
+	name: string
+	main: {
+		feels_like: number
+		humidity: number
+		pressure: number
+		temp: number
+		temp_max: number
+		temp_min: number
+	}
+	weather: {
+		description: string
+		icon: string
+		id: number
+		main: string
+	}[]
+}
+
+export async function fetchOpenWeatherData(city: string): Promise<OpenWeatherData> {
 	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`)
 	if(!res.ok) throw new Error('City not found')
-	const data = await res.json()
+	const data: OpenWeatherData = await res.json();
 	return data;
 }
